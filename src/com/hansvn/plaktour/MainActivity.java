a441367a039fed28ceeab174eba7c0f05e521c4a
hansvn <hans_vn@hotmail.com>
2013-04-05 21:50:48 +0200

    MainActivity - ListView & BottomBar
    listview en een bottom(action)bar toegevoegd aan de mainactivity
    (startscherm app).

--- src/com/hansvn/plaktour/MainActivity.java
+++ src/com/hansvn/plaktour/MainActivity.java
@@ -3,6 +3,7 @@ package com.hansvn.plaktour;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.ListView;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +11,8 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		initToursList();
 	}
 
 	@Override
@@ -17,6 +20,11 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+	public void initToursList() {
+		ListView toursList = (ListView) findViewById(R.id.listViewTours);
+		toursList.setBackgroundColor(0x552222FF);
 	}
 
 }

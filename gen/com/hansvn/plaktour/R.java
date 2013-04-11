032502dc0ab545a2a54f2707139ed1f387735cdb
hansvn <hans_vn@hotmail.com>
2013-04-11 17:12:52 +0200

    Layout detail en MainActivity
    layouts van deze twee schermen, en een heel klein beetje code

--- gen/com/hansvn/plaktour/R.java
+++ gen/com/hansvn/plaktour/R.java
@@ -10,6 +10,27 @@ package com.hansvn.plaktour;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int aqua=0x7f040009;
+        public static final int black=0x7f04000f;
+        public static final int blue=0x7f04000d;
+        public static final int fuchsia=0x7f040002;
+        public static final int gray=0x7f040005;
+        public static final int green=0x7f04000c;
+        public static final int lime=0x7f04000a;
+        public static final int maroon=0x7f040008;
+        public static final int navy=0x7f04000e;
+        public static final int olive=0x7f040006;
+        public static final int purple=0x7f040007;
+        public static final int red=0x7f040003;
+        public static final int silver=0x7f040004;
+        public static final int teal=0x7f04000b;
+        public static final int tourDetailLabel=0x7f040011;
+        public static final int tourDetailTitle=0x7f040010;
+        public static final int tourDetailValue=0x7f040012;
+        public static final int white=0x7f040000;
+        public static final int yellow=0x7f040001;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -17,26 +38,60 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int comments_icon=0x7f020000;
+        public static final int gradient_background=0x7f020001;
+        public static final int gradient_tours=0x7f020002;
+        public static final int ic_launcher=0x7f020003;
+        public static final int newtour_icon=0x7f020004;
+        public static final int start_icon=0x7f020005;
     }
     public static final class id {
-        public static final int action_settings=0x7f080002;
-        public static final int bottomActionBar=0x7f080001;
-        public static final int listViewTours=0x7f080000;
+        public static final int action_settings=0x7f090014;
+        public static final int bottomActionBar=0x7f090001;
+        public static final int detail_lastActivityLabel=0x7f09000f;
+        public static final int detail_lastActivityLayout=0x7f09000e;
+        public static final int detail_lastActivityValue=0x7f090010;
+        public static final int detail_pointsDoneLabel=0x7f090012;
+        public static final int detail_pointsDoneLayout=0x7f090011;
+        public static final int detail_pointsDoneValue=0x7f090013;
+        public static final int detail_pointsLabel=0x7f090006;
+        public static final int detail_pointsLayout=0x7f090005;
+        public static final int detail_pointsValue=0x7f090007;
+        public static final int detail_postersLabel=0x7f090009;
+        public static final int detail_postersLayout=0x7f090008;
+        public static final int detail_postersValue=0x7f09000a;
+        public static final int detail_timeLabel=0x7f09000c;
+        public static final int detail_timeLayout=0x7f09000b;
+        public static final int detail_timeValue=0x7f09000d;
+        public static final int detail_title=0x7f090004;
+        public static final int listViewTours=0x7f090000;
+        public static final int tourDetailBackground=0x7f090002;
+        public static final int tourDetailLayout=0x7f090003;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int activity_tour_detail=0x7f030001;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
+        public static final int action_settings=0x7f060001;
+        public static final int app_name=0x7f060000;
+        public static final int comments_button=0x7f060008;
+        public static final int detail_lastActivity=0x7f060005;
+        public static final int detail_points=0x7f060002;
+        public static final int detail_pointsDone=0x7f060006;
+        public static final int detail_posters=0x7f060003;
+        public static final int detail_time=0x7f060004;
+        public static final int newTour_button=0x7f060009;
+        /**  Accessible strings 
+         */
+        public static final int start_button=0x7f060007;
     }
     public static final class style {
         /** 
@@ -60,10 +115,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
--- src/com/hansvn/plaktour/MainActivity.java
+++ src/com/hansvn/plaktour/MainActivity.java
@@ -1,12 +1,18 @@
 package com.hansvn.plaktour;
 
+import java.util.ArrayList;
+
+import android.os.Build;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
 import android.widget.ListView;
 
 public class MainActivity extends Activity {
 
+	public final static String EXTRA_MESSAGE = "com.hansvn.plaktour.MESSAGE";
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -24,7 +30,11 @@ public class MainActivity extends Activity {
 	
 	public void initToursList() {
 		ListView toursList = (ListView) findViewById(R.id.listViewTours);
-		toursList.setBackgroundColor(0x552222FF);
+		ArrayList<View> tours = new ArrayList<View>();
+		tours.add(findViewById(R.id.bottomActionBar));
+		if (Build.VERSION.SDK_INT >= 16) {
+			toursList.addView((View)findViewById(R.id.bottomActionBar));
+		}
 	}
 
 }

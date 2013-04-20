6c53f043c43a77bf85f988125afed2609232522d
hansvn <hans_vn@hotmail.com>
2013-04-20 13:51:44 +0200

    Swipen en bottomActionBar buttons
    swipen op listItems om details te bekijken toegevoegd.
    De knoppen in de bottomActionBar hun functies gebonden.

--- gen/com/hansvn/plaktour/R.java
+++ gen/com/hansvn/plaktour/R.java
@@ -28,6 +28,7 @@ public final class R {
         public static final int tourDetailLabel=0x7f040011;
         public static final int tourDetailTitle=0x7f040010;
         public static final int tourDetailValue=0x7f040012;
+        public static final int tourListItem_normal=0x7f040013;
         public static final int white=0x7f040000;
         public static final int yellow=0x7f040001;
     }
@@ -48,31 +49,39 @@ public final class R {
         public static final int ic_launcher=0x7f020003;
         public static final int newtour_icon=0x7f020004;
         public static final int start_icon=0x7f020005;
+        public static final int tour_list_selector=0x7f020006;
+        public static final int tour_list_selector_focussed=0x7f020007;
+        public static final int tour_list_selector_normal=0x7f020008;
+        public static final int tour_list_selector_pressed=0x7f020009;
     }
     public static final class id {
-        public static final int action_settings=0x7f090016;
+        public static final int action_settings=0x7f09001a;
         public static final int bottomActionBar=0x7f090001;
-        public static final int description_view=0x7f090015;
-        public static final int detail_lastActivityLabel=0x7f09000f;
-        public static final int detail_lastActivityLayout=0x7f09000e;
-        public static final int detail_lastActivityValue=0x7f090010;
-        public static final int detail_pointsDoneLabel=0x7f090012;
-        public static final int detail_pointsDoneLayout=0x7f090011;
-        public static final int detail_pointsDoneValue=0x7f090013;
-        public static final int detail_pointsLabel=0x7f090006;
-        public static final int detail_pointsLayout=0x7f090005;
-        public static final int detail_pointsValue=0x7f090007;
-        public static final int detail_postersLabel=0x7f090009;
-        public static final int detail_postersLayout=0x7f090008;
-        public static final int detail_postersValue=0x7f09000a;
-        public static final int detail_timeLabel=0x7f09000c;
-        public static final int detail_timeLayout=0x7f09000b;
-        public static final int detail_timeValue=0x7f09000d;
-        public static final int detail_title=0x7f090004;
+        public static final int comments_button=0x7f090002;
+        public static final int description_view=0x7f090019;
+        public static final int detail_lastActivityLabel=0x7f090012;
+        public static final int detail_lastActivityLayout=0x7f090011;
+        public static final int detail_lastActivityValue=0x7f090013;
+        public static final int detail_pointsDoneLabel=0x7f090015;
+        public static final int detail_pointsDoneLayout=0x7f090014;
+        public static final int detail_pointsDoneValue=0x7f090016;
+        public static final int detail_pointsLabel=0x7f090009;
+        public static final int detail_pointsLayout=0x7f090008;
+        public static final int detail_pointsValue=0x7f09000a;
+        public static final int detail_postersLabel=0x7f09000c;
+        public static final int detail_postersLayout=0x7f09000b;
+        public static final int detail_postersValue=0x7f09000d;
+        public static final int detail_timeLabel=0x7f09000f;
+        public static final int detail_timeLayout=0x7f09000e;
+        public static final int detail_timeValue=0x7f090010;
+        public static final int detail_title=0x7f090007;
+        public static final int listItem_layout=0x7f090017;
         public static final int listViewTours=0x7f090000;
-        public static final int title_view=0x7f090014;
-        public static final int tourDetailBackground=0x7f090002;
-        public static final int tourDetailLayout=0x7f090003;
+        public static final int new_tour_button=0x7f090004;
+        public static final int start_button=0x7f090003;
+        public static final int title_view=0x7f090018;
+        public static final int tourDetailBackground=0x7f090005;
+        public static final int tourDetailLayout=0x7f090006;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
--- src/com/hansvn/plaktour/MainActivity.java
+++ src/com/hansvn/plaktour/MainActivity.java
@@ -7,39 +7,86 @@ import android.view.Menu;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
+import android.widget.AdapterView.OnItemLongClickListener;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 	public final static String SELECTED_TOUR = "com.hansvn.plaktour.TOUR";
 	public static TourListAdapter tourListAdapter;
+	private static int selectedTour;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
-		ListView toursListView = (ListView) findViewById(R.id.listViewTours);
+		//set data on listview
+		final ListView toursListView = (ListView) findViewById(R.id.listViewTours);
 		tourListAdapter = new TourListAdapter();
 		toursListView.setAdapter(tourListAdapter);
+				
+		//final swipedetector to access it from the onItemClick function
+		final SwipeDetector swipeDetector = new SwipeDetector();
+		toursListView.setOnTouchListener(swipeDetector);
 		
 		toursListView.setOnItemClickListener(new OnItemClickListener() {
 			@Override
 			public void onItemClick(AdapterView<?> adapter, View view, int position, long arg){
-				//testmessage:
-				//Toast.makeText(getApplicationContext(), "You clicked at item "+ position, Toast.LENGTH_LONG).show();
-				
-				//open tourdetail met item dat op positie position staat...
-				//intent enzovoort
-				
-				Intent intent = new Intent(view.getContext(), TourDetailActivity.class);
-				intent.putExtra(SELECTED_TOUR, position+"");
-				startActivity(intent);
+	            if (swipeDetector.swipeDetected()){
+	                // do the onSwipe action 
+	            	Intent intent = new Intent(view.getContext(), TourDetailActivity.class);
+					intent.putExtra(SELECTED_TOUR, Integer.toString(position));
+					startActivity(intent);
+	            } else {
+	                // do the onItemClick action
+	            	//Toast.makeText(getApplicationContext(), "You clicked at item "+ position, Toast.LENGTH_LONG).show();
+	            	view.setSelected(true);
+	            	selectedTour = position;
+	            	//een animatieke op achtergrond:
+	            	/*
+	            	LinearLayout listItem = (LinearLayout) view.findViewById(R.id.listItem_layout);
+	            	listItem.setBackgroundDrawable(background)
+	            	*/
+	            	//een animatie-list maken om op achtergrond te zetten
+	            	//animatie verwijderen van zodra de onclick stopt...
+	            }
 			}
 		});
 		
-		
+		toursListView.setOnItemLongClickListener(new OnItemLongClickListener() {
+	        @Override
+	        public boolean onItemLongClick(AdapterView<?> adapter, View view, int position, long arg) {
+	            if (swipeDetector.swipeDetected()){
+	                // do the onSwipe action 
+	            	Toast.makeText(getApplicationContext(), "You Longswiped at item "+ position, Toast.LENGTH_LONG).show();
+	            } else {
+	                // do the onItemLongClick action
+	            	Toast.makeText(getApplicationContext(), "You LongClicked at item "+ position, Toast.LENGTH_LONG).show();
+	            }
+	            return true;
+	        }
+	    });
 	}
-
+	
+	//actie van start tour button onclick staat in xml
+	public void startTour(View view) {
+		//get te tour to pass:
+		Tour tourToOpen = (Tour) tourListAdapter.getItem(selectedTour);
+		//test message:
+		Toast.makeText(getApplicationContext(), "the selected tour: "+ tourToOpen.getTitle(), Toast.LENGTH_LONG).show();
+	}
+	
+	//actie van comments button
+	public void viewTourComments(View view) {
+		Toast.makeText(getApplicationContext(), "you clicked the comments button", Toast.LENGTH_LONG).show();
+	}
+	
+	//actie van newTour button
+	public void newTour(View view) {
+		Toast.makeText(getApplicationContext(), "you clicked the new tour", Toast.LENGTH_LONG).show();
+	}
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
